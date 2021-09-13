e37f722b7c748479183494f49f43ac52164b0f8f
Vishal <vishal.aditya6@gmail.com>
2021-09-13 17:25:12 +0530

    Assignment 1 complete

---
+++ src/main/java/com/example/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,19 @@
+package com.example.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private Ingredient ingredient;
+
+}
