a3005037a95cd5854318a044007f83ce40be78e6
Vishal <vishal.aditya6@gmail.com>
2021-09-28 00:18:21 +0530

    Add image from Database.

--- src/main/java/com/example/spring5recipeapp/controllers/ImageController.java
+++ src/main/java/com/example/spring5recipeapp/controllers/ImageController.java
@@ -1,11 +1,18 @@
 package com.example.spring5recipeapp.controllers;
 
+import com.example.spring5recipeapp.commands.RecipeCommand;
 import com.example.spring5recipeapp.services.ImageService;
 import com.example.spring5recipeapp.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -30,4 +37,24 @@ public class ImageController {
         imageService.saveImageFile(Long.valueOf(id), file);
         return "redirect:/recipe/" + id + "/show";
     }
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+
+            for (Byte wrappedByte : recipeCommand.getImage()) {
+                byteArray[i++] = wrappedByte;
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
+    }
+
 }
