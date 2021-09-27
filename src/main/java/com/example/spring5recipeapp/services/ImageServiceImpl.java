2696ec7ec261b5fdf7c97aef9446f997f555fcc1
Vishal <vishal.aditya6@gmail.com>
2021-09-27 23:57:30 +0530

    Add image to database.

--- src/main/java/com/example/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/example/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,15 +1,42 @@
 package com.example.spring5recipeapp.services;
 
+import com.example.spring5recipeapp.domain.Recipe;
+import com.example.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @Slf4j
 public class ImageServiceImpl implements ImageService{
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        log.debug("Recived a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error Occurred " + e);
+            e.printStackTrace();
+        }
     }
 }
