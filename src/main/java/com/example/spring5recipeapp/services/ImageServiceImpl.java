698bbf6ef40776a44645451f48e8a052168bf737
Vishal <vishal.aditya6@gmail.com>
2021-09-27 23:40:59 +0530

    Add ImageController and ImageService

---
+++ src/main/java/com/example/spring5recipeapp/services/ImageServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.spring5recipeapp.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@Slf4j
+public class ImageServiceImpl implements ImageService{
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        log.debug("Recived a file");
+    }
+}
